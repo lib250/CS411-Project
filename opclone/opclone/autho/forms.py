34fddd78c1378354869cb09231349f019268cb08
RicardoA Alvarado <74122085+RicardoAlvaradoo@users.noreply.github.com>
2022-12-05 18:47:02 -0500

    Delete opclone/opclone directory

--- opclone/opclone/autho/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from .models import Player
-
-class NameForm(ModelForm):
-   class Meta:
-        model = Player
-        fields = '__all__'
--- opclone/opclone/autho/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('search', views.index, name='index'),
-    path('data', views.ritoAPI, name = 'ApiCode'),
-    path('new', views.get_summoner, name ='test' )
-]
