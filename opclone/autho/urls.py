861f909364dadb60646dcbbd321f02f1120bdf87
RicardoAlvaradoo <ricky200213@gmail.com>
2022-12-05 20:08:07 -0500

    have api displaying data on frontend

--- opclone/autho/urls.py
+++ opclone/autho/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('search', views.index, name='index'),
-    path('data', views.ritoAPI, name = 'ApiCode'),
+    
     path('new', views.get_summoner, name ='test' )
 ]
--- opclone/autho/views.py
+++ opclone/autho/views.py
@@ -6,6 +6,7 @@ from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 # Create your views here.
 from .forms import NameForm
+import urllib.parse
 def index(request):
     template = loader.get_template('input.html')
 
@@ -13,24 +14,35 @@ def index(request):
 #RGAPI-41cbb789-b621-4522-acef-e5211ecfc836
 #Regions, inputted lower case
 #EUN1, EUW1	JP1	KR	LA1	LA2 NA1	OC1	TR1	RU BR1
-def ritoAPI(request):
-    api_key = 'RGAPI-41cbb789-b621-4522-acef-e5211ecfc836'
-    watcher = LolWatcher(api_key)
-    form = NameForm(request.POST)
-    name = form.cleaned_data['your_name']
-    my_region = form.cleaned_data['your_region']
-    me = watcher.summoner.by_name(my_region, name)
-    ranked_stats = watcher.league.by_summoner(my_region, me['id'])
-    return render(request, 'display.html', {'response': ranked_stats})
+
 
 def get_summoner(request):
+    #checks valididy of form
     if request.method == 'POST':
         form = NameForm(request.POST)
+        if form.is_valid():
+            params = {
+                "your_name" : form.cleaned_data['name'],
+                "your_region" : form.cleaned_data['region']
+            }
+        q_string = urllib.parse.urlencode(params)
         
-        form.save()
-       # full = NameForm(your_name = name, your_region = region)
-       #waaa
-        return redirect('/autho/data')
+       
+        nameregiondict = q_string
+        #gets name from qstring
+        name = nameregiondict.split("&")
+        name = name[0].split("=")
+        name = name[1] 
+        #gets region from qstring
+        my_region = nameregiondict.split("&")
+        my_region = my_region[1].split("=")
+        my_region = my_region[1]
+        #riot api form, gets data
+        api_key = 'RGAPI-2df33002-8998-4de1-bbce-c62b60f8dc96'
+        watcher = LolWatcher(api_key)
+        me = watcher.summoner.by_name(my_region, name)
+        ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+        return render(request, 'display.html', {'response': ranked_stats})
         #if form.is_valid():
            # name = form.cleaned_data['your_name']
             #region = form.cleaned_data['your_region'] 
